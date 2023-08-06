7753a9c5283ac1f51b3e24530a0d242dac44ea24
mikalikahi <sweeney.phil@gmail.com>
2023-08-06 12:18:25 +0800

    Rewrote test set up with only one non-async beforeEach for PostDetailComponent -- try again to understand

--- Angular-Testing-Tutorial/src/app/components/post-detail/post-detail.component.spec.ts
+++ Angular-Testing-Tutorial/src/app/components/post-detail/post-detail.component.spec.ts
@@ -7,23 +7,6 @@ import { PostDetailComponent } from './post-detail.component';
 import { ActivatedRoute } from '@angular/router';
 
 fdescribe('PostDetailComponent', () => {
-  let postService: any;
-  let location: any;
-  let route: any;
-  
-  /*
-  let mockActivatedRoute = {
-    snapshot: {
-      paramMap: {
-        get: () => {
-          return '3';
-        }
-      }
-    }
-  };
-  let mockPostService = jasmine.createSpyObj(['getPost', 'updatePost']);
-  let mockLocation = jasmine.createSpyObj(['back']);
-  */
   let POSTS = [
     {
       id: 1,
@@ -41,59 +24,32 @@ fdescribe('PostDetailComponent', () => {
       title: 'title 3',
     },
   ];
-  let component: PostDetailComponent;
   let fixture: ComponentFixture<PostDetailComponent>;
-  let el: DebugElement;
-  let htmlElement: HTMLElement;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     let mockActivatedRoute = {
       snapshot: {
         paramMap: {
           get: () => {
             return '3';
-          }
-        }
-      }
+          },
+        },
+      },
     };
+
     let mockPostService = jasmine.createSpyObj(['getPost', 'updatePost']);
     let mockLocation = jasmine.createSpyObj(['back']);
 
-    await TestBed.configureTestingModule({
-      declarations: [ PostDetailComponent ],
+    TestBed.configureTestingModule({
+      declarations: [PostDetailComponent],
       providers: [
         { provide: Location, useValue: mockLocation },
-        { provide: PostService, useValue: mockPostService},
-        { provide: ActivatedRoute, useValue: mockActivatedRoute  }
-      ]
-    })
-    .compileComponents();
-  });
+        { provide: PostService, useValue: mockPostService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PostDetailComponent);
-    postService = TestBed.inject(PostService);
-    location = TestBed.inject(Location);
-    component = fixture.componentInstance;
-    el = fixture.debugElement;
-    htmlElement = el.query(By.css('h2')).nativeElement as HTMLElement;
-    postService.getPost.and.returnValue(of(POSTS[2]));
-    location.back.and.returnValue('');
-    fixture.detectChanges();
   });
 
-  it('should render the post title in the h2 template',() => { //   fakeAsync(
-    //fixture.detectChanges();
-    //flush();
-    //const h2Content = el.query(By.css("h2"));
-    //const element = fixture.debugElement.query(By.css('h2')).nativeElement as HTMLElement;
-    //fixture.detectChanges();
-    //flush();
-    //expect(h2Content.nativeElement.textContent).toContain("title 3");
-  expect(htmlElement.textContent).toBe(fixture.componentInstance.post.title);
-  }); //)
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
